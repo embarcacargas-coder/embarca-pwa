ba2344f1e4b8d7db6bac0a2978093291540e2600
embarcacargas-coder <embarcacargas@gmail.com>
2026-02-04 17:40:36 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,7 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener("activate", event => {
+  self.clients.claim();
 });
